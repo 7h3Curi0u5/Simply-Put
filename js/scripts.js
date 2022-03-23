abd422d3e9f4e4075c1ff146d93e33f6774d1063
7h3Curi0u5 <rlopez4599@gmail.com>
2022-03-23 10:02:49 -0400

    First Pokemon
    Added pokemon objects to the code with 3 values

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 
 let pokemonList = [];
+
+let Mewtwo = {
+  name: 'Mewtwo',
+  height: 2,
+  types: ['Psychic']
+}
+
+let Lucario = {
+  name: 'Lucario',
+  height: 1.2,
+  types: ['steel','fighting']
+}
