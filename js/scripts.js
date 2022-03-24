0f9600fd1eac7b063fd50d4ac5f48ef390666ab6
7h3Curi0u5 <rlopez4599@gmail.com>
2022-03-24 09:46:32 -0400

    Revision to array
    Added objects properly into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
 
-let pokemonList = [];
-
-let Mewtwo = {
+const array = [{
   name: 'Mewtwo',
   height: 2,
   types: ['Psychic']
-}
-
-let Lucario = {
+},
+{
   name: 'Lucario',
   height: 1.2,
   types: ['steel','fighting']
-}
-
-let Rayquaza = {
+},
+{
   name: 'Rayquaza',
   height: 7,
   types: ['Dragon','Flying']
-}
-
-let Reshiram = {
+},
+{
   name: 'Reshiram',
   height:3.2,
   types: ['Dragon','Fire']
-}
+}]
